0998d9aa3e990c70a4a0aaa37b50ac699cd98fb4
Hmatata-254 <husseinmatata591@gmail.com>
2025-02-11 16:37:40 +0300

    server commit

---
+++ spring_backend/SpringServer/src/main/groovy/com/example/springServer/SpringServerApplication.groovy
@@ -0,0 +1,13 @@
+package com.example.springServer
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SpringServerApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(SpringServerApplication, args)
+	}
+
+}
